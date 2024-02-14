7fe6e1aa00a7b96fa395ff938aefffc1cfa787b9
Artem Timoshenko <atimoshenko@ubic.tech>
2024-02-14 09:11:03 +0300

    ASTONJS-1 [feat]: test

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,8 @@ import classes from './Footer.module.css';
 function Footer() {
   return (
     <div className={classes.footer}>
-      <p className="mb-0 mt-1">Если хотите со мной связаться:</p>
+      {/* stash 0 */}
+      {/* <p className="mb-0 mt-1">Если хотите со мной связаться:</p> */}
       <a href="https://github.com/tymphoto" className={classes.footerLink}>Артем Тимошенко</a>
       <p>Test commit</p>
     </div>
